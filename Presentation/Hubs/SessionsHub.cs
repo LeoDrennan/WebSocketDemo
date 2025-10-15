4257de68cb1893a1730c8ed3dcccc867108ffc95
Leo Drennan <drennanleo@gmail.com>
2025-10-16 00:10:47 +0100

    Add data broadcast when in OnConnected method in SessionsHub

--- Presentation/Hubs/SessionsHub.cs
+++ Presentation/Hubs/SessionsHub.cs
@@ -1,6 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Domain.Models;
+using Infrastructure.Sessions.Abstractions;
+using Microsoft.AspNetCore.SignalR;
+using Shared.Sessions.Abstractions;
 
 namespace Presentation.Hubs
 {
-    public class SessionsHub : Hub { }
+    public class SessionsHub : Hub
+    {
+        private readonly ISessionsClient _sessionsClient;
+        private readonly ISessionsBroadcastService _sessionsBroadcastService;
+
+        public SessionsHub(ISessionsClient sessionsClient, ISessionsBroadcastService sessionsBroadcastService)
+        {
+            _sessionsClient = sessionsClient ?? throw new ArgumentNullException(nameof(sessionsClient));
+            _sessionsBroadcastService = sessionsBroadcastService ?? throw new ArgumentNullException(nameof(sessionsBroadcastService));
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            List<SessionDetailDto> sessionDetails = await _sessionsClient.GetCurrentStateAsync(Context.ConnectionAborted);
+            await _sessionsBroadcastService.BroadcastUpdateAsync(sessionDetails, Context.ConnectionAborted);
+
+            await base.OnConnectedAsync();
+        }
+    }
 }
