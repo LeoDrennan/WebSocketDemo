a0ecbdedcbbacee99d8aa5b2098afc6334c9d25a
Leo Drennan <drennanleo@gmail.com>
2025-10-16 22:23:54 +0100

    Remove session immediately fetching data

--- Presentation/Hubs/RaceSessionHub.cs
+++ Presentation/Hubs/RaceSessionHub.cs
@@ -9,15 +9,10 @@ namespace Presentation.Hubs
     public class RaceSessionHub : Hub
     {
         private readonly IRaceSessionService _raceSessionService;
-        private readonly IRaceSessionClient _raceSessionClient;
-        private readonly IRaceSessionBroadcastService _raceSessionBroadcastService;
 
-        public RaceSessionHub(IRaceSessionService raceSessionService, IRaceSessionClient raceSessionClient,
-            IRaceSessionBroadcastService raceSessionBroadcastService)
+        public RaceSessionHub(IRaceSessionService raceSessionService)
         {
             _raceSessionService = raceSessionService ?? throw new ArgumentNullException(nameof(raceSessionService));
-            _raceSessionClient = raceSessionClient ?? throw new ArgumentNullException(nameof(raceSessionClient));
-            _raceSessionBroadcastService = raceSessionBroadcastService ?? throw new ArgumentNullException(nameof(raceSessionBroadcastService));
         }
 
         public override async Task OnConnectedAsync()
@@ -31,26 +26,12 @@ namespace Presentation.Hubs
             }
 
             await _raceSessionService.SubscribeAsync(Context.ConnectionId, sessionId!, Context.ConnectionAborted);
-
-            await SendCurrentSessionDetails(sessionId);
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             _raceSessionService.UnsubscribeAsync(Context.ConnectionId);
             await base.OnDisconnectedAsync(exception);
-        }
-
-        private async Task SendCurrentSessionDetails(string sessionId)
-        {
-            RaceSessionDto? dto = await _raceSessionClient.GetCurrentStateAsync(sessionId, Context.ConnectionAborted);
-
-            if (dto == null)
-            {
-                return;
-            }
-
-            await _raceSessionBroadcastService.BroadcastUpdateAsync(sessionId, dto, Context.ConnectionAborted);
         }
     }
 }
