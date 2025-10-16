71aca6abc26715ff6c1eb3c366d6062aa047f741
Leo Drennan <drennanleo@gmail.com>
2025-10-16 01:04:56 +0100

    Add OnConnected behaviour to RaceSessionHub to send details

--- Presentation/Hubs/RaceSessionHub.cs
+++ Presentation/Hubs/RaceSessionHub.cs
@@ -1,15 +1,23 @@
 ﻿using Application.RaceSession.Abstractions;
+using Domain.Models;
+using Infrastructure.RaceSession.Abstractions;
 using Microsoft.AspNetCore.SignalR;
+using Shared.RaceSession.Abstractions;
 
 namespace Presentation.Hubs
 {
     public class RaceSessionHub : Hub
     {
         private readonly IRaceSessionService _raceSessionService;
+        private readonly IRaceSessionClient _raceSessionClient;
+        private readonly IRaceSessionBroadcastService _raceSessionBroadcastService;
 
-        public RaceSessionHub(IRaceSessionService raceSessionService)
+        public RaceSessionHub(IRaceSessionService raceSessionService, IRaceSessionClient raceSessionClient,
+            IRaceSessionBroadcastService raceSessionBroadcastService)
         {
-            _raceSessionService = raceSessionService;
+            _raceSessionService = raceSessionService ?? throw new ArgumentNullException(nameof(raceSessionService));
+            _raceSessionClient = raceSessionClient ?? throw new ArgumentNullException(nameof(raceSessionClient));
+            _raceSessionBroadcastService = raceSessionBroadcastService ?? throw new ArgumentNullException(nameof(raceSessionBroadcastService));
         }
 
         public override async Task OnConnectedAsync()
@@ -23,12 +31,26 @@ namespace Presentation.Hubs
             }
 
             await _raceSessionService.SubscribeAsync(Context.ConnectionId, sessionId!, Context.ConnectionAborted);
+
+            await SendCurrentSessionDetails(sessionId);
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             _raceSessionService.UnsubscribeAsync(Context.ConnectionId);
             await base.OnDisconnectedAsync(exception);
+        }
+
+        private async Task SendCurrentSessionDetails(string sessionId)
+        {
+            RaceSessionDto? dto = await _raceSessionClient.GetCurrentStateAsync(sessionId, Context.ConnectionAborted);
+
+            if (dto == null)
+            {
+                return;
+            }
+
+            await _raceSessionBroadcastService.BroadcastUpdateAsync(sessionId, dto, Context.ConnectionAborted);
         }
     }
 }
